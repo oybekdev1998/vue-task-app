4d57f8430af7e92021218d29d10007ed76468ddb
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-10-16 21:31:22 +0500

    Add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button :style="{background: color}" class="btn">{{text}}</button>
+</template>
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String
+  }
+  
+}
+</script>
+
+<style>
+.btn {
+  background: green;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 400;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 15px;
+}
+</style>
