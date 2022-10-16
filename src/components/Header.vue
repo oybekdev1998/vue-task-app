6405017e5e495fa3d1b3cba24fef19c7f9f5af0e
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-10-16 21:31:45 +0500

    Add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <h1>{{title}}</h1>
+    <Button text="Add Task" color="green" />
+  </div>
+</template>
+
+<script>
+import Button from './Button.vue';
+export default {
+    name: "Header",
+    props: {
+      title: String
+    },
+    components: { Button }
+}
+</script>
+
+<style>
+.header {
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
+
+h1 {
+  font-size: 24px;
+  color: #000;
+  
+}
+
+</style>
